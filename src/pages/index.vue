1432120b14fb649677ef9fc6014893953b4d7fb6
Ibis <ibis.xin@gmail.com>
2024-12-23 19:36:52 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card>
+    <v-card-title> Title </v-card-title>
+    <v-card-subtitle> Subtitle </v-card-subtitle>
+    <v-card-actions>
+      <v-btn> Button </v-btn>
+    </v-card-actions>
+  </v-card>
+  <!-- <HelloWorld /> -->
+</template>
+
+<script lang="ts" setup>
+//
+</script>
